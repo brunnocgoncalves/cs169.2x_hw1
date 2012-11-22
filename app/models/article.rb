ca022355f17c7522b9d2d13801434eeff685f6c7
Harish Narayanan <hnarayanan@gmail.com>
2012-11-22 16:57:28 +0100

    Attempt to merge comments as well (untested)

--- app/models/article.rb
+++ app/models/article.rb
@@ -104,6 +104,26 @@ class Article < Content
                                       :author => article_1.author,
                                       :body => article_1.body + article_2.body,
                                       :user_id => article_1.user_id)
+      comments_1 = Feedback.find_all_by_article_id(article_1_id)
+      comments_2 = Feedback.find_all_by_article_id(article_2_id)
+
+      unless comments_1.blank?
+        comments_1.each do |comment|
+          comment.article_id = merged_article_id
+          comment.save
+        end
+      end
+
+      unless comments_2.blank?
+        comments_2.each do |comment|
+          comment.article_id = merged_article_id
+          comment.save
+        end
+      end
+
+      Article.destroy(article_1_id)
+      Article.destroy(article_2_id)
+      merged_article
     end
 
     def last_draft(article_id)
